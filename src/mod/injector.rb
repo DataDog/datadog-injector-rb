d2ccb56a2eebf17fa780710ee6017d098895e893
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2025-06-29 18:21:15 +0200

    Set variables for Bundler to pick up injection results

--- src/mod/injector.rb
+++ src/mod/injector.rb
@@ -12,30 +12,32 @@ class << self
     # TODO: check if nested injection (maybe very early too)
     # TODO: check if injection already performed
 
-    # parse lock file
-    CONTEXT.isolate do
-      BUNDLER.send(:require!)
+    package_basepath = ENV['DD_INTERNAL_RUBY_INJECTOR_BASEPATH'] || File.expand_path(File.join(File.dirname(__FILE__), '..'))
+    package_gem_home = ENV['DD_INTERNAL_RUBY_INJECTOR_GEM_HOME'] || File.join(package_basepath, RUBY.api_version)
+    package_lockfile = ENV['DD_INTERNAL_RUBY_INJECTOR_LOCKFILE'] || File.join(package_gem_home, 'Gemfile.lock')
 
-      package_basepath = ENV['DD_INTERNAL_RUBY_INJECTOR_BASEPATH'] || File.expand_path(File.join(File.dirname(__FILE__), '..'))
-      package_gem_home = ENV['DD_INTERNAL_RUBY_INJECTOR_GEM_HOME'] || File.join(package_basepath, RUBY.api_version)
-      package_lockfile = ENV['DD_INTERNAL_RUBY_INJECTOR_LOCKFILE'] || File.join(package_gem_home, 'Gemfile.lock')
+    # TODO: these are in context
+    # pinpoint app gemfile and lockfile
+    app_gemfile  = Bundler.default_gemfile
+    app_lockfile = Bundler.default_lockfile
+
+    # determine output paths
+    # out = File.join(app_gemfile.dirname, 'tmp', 'datadog')
+    out = File.join(app_gemfile.dirname)
+
+    # TODO: hash path + content to detect changes
+    datadog_gemfile  = File.join(out, 'datadog.gemfile')
+    datadog_lockfile = File.join(out, 'datadog.gemfile.lock')
+
+    success = CONTEXT.isolate do
+
+      ENV['GEM_PATH'] = package_gem_home
+
+      BUNDLER.send(:require!)
 
       # TODO: this could be gathered in context
       # list gems packaged for injection
       package_locked = Bundler::LockfileParser.new(Bundler.read_file(package_lockfile))
-
-      # TODO: these are in context
-      # pinpoint app gemfile and lockfile
-      app_gemfile  = Bundler.default_gemfile
-      app_lockfile = Bundler.default_lockfile
-
-      # determine output paths
-      # out = File.join(app_gemfile.dirname, 'tmp', 'datadog')
-      out = File.join(app_gemfile.dirname)
-
-      # TODO: hash path + content to detect changes
-      datadog_gemfile  = File.join(out, 'datadog.gemfile')
-      datadog_lockfile = File.join(out, 'datadog.gemfile.lock')
 
       File.write(datadog_gemfile, File.read(app_gemfile))
       File.write(datadog_lockfile, File.read(app_lockfile))
@@ -48,10 +50,15 @@ class << self
 
       true
     end
+
+    if success
+      Gem.paths = { 'GEM_PATH' => "#{package_gem_home}:#{ENV['GEM_PATH']}" }
+      ENV['GEM_PATH'] = Gem.path.join(File::PATH_SEPARATOR)
+      ENV['BUNDLE_GEMFILE'] = datadog_gemfile
+    end
   end
 
   def options_for(name)
     name == 'datadog' ? { 'require' => 'datadog/single_step_instrument' } : {}
   end
 end
-

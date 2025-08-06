9c03ff65cbef6234e9cc549543608cce94a82f57
Sydney Tung <sydney.tung@datadoghq.com>
2025-08-06 09:23:00 -0400

    Add result, result_class, and result_reason to metadata
    fix
    
    remove tests that don't work
    
    add metadata results

--- src/mod/main.rb
+++ src/mod/main.rb
@@ -29,7 +29,7 @@ if (result = guard.call(context.status))
 
   telemetry.emit([
     { :name => 'library_entrypoint.abort', :tags => tags },
-  ])
+  ], { :result => 'abort', :result_reason => 'library_entrypoint.abort', :result_class => 'internal_error' })
 
 # stage 3: inject
 else
@@ -42,7 +42,7 @@ else
 
     telemetry.emit([
       { :name => 'library_entrypoint.complete', :tags => ["reason:internal.skip"] },
-    ])
+    ], { :result => 'abort', :result_reason => 'inject:skip', :result_class => 'internal_error' })
   else
     begin
       # TODO: pass args, e.g context, location, etc...
@@ -51,20 +51,20 @@ else
       if err
         telemetry.emit([
           { :name => 'library_entrypoint.error', :tags => ["reason:#{err}"] },
-        ])
+        ], { :result => 'error', :result_reason => err.to_s, :result_class => 'internal_error' })
       else
         log.info { 'inject:complete' }
 
         telemetry.emit([
           { :name => 'library_entrypoint.complete' },
-        ])
+        ], { :result => 'success', :result_reason => 'Successfully configured ddtrace package', :result_class => 'success' })
       end
     rescue StandardError => _e
       log.info { 'inject:error' }
 
       telemetry.emit([
         { :name => 'library_entrypoint.error', :tags => ["reason:exc.fatal"] },
-      ])
+      ], { :result => 'error', :result_reason => 'library_entrypoint.error', :result_class => 'internal_error' })
     end
   end
 end

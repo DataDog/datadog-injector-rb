f33153ff458b76c5baefe797723411dc4d6bd572
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2025-10-28 12:54:18 +0100

    Report descriptive reason texts

--- src/mod/report.rb
+++ src/mod/report.rb
@@ -17,11 +17,44 @@ class << self
     end
   end
 
+  def text_for(reason, value)
+    case reason
+    when nil
+      nil
+    when 'runtime.parser'
+      "The Ruby runtime language parser cannot parse the injected code (expected:2.4+ actual:#{value})"
+    when 'runtime.version'
+      "The Ruby runtime language version is insufficient to execute the injected code (expected:2.6+ actual:#{value})"
+    when 'runtime.engine'
+      "The Ruby runtime engine (#{value}) is not compatible"
+    when 'runtime.forkless'
+      "The Ruby runtime does not support forking"
+    when 'rubygems.version'
+      "The Ruby runtime 'rubygems' component version in use is too low (expected:[3.4+,<4.0] actual:#{value})"
+    when 'bundler.version'
+      "The Ruby runtime 'bundler' component version in use is out of range (expected:[2.4+,<4.0] actual:#{value})"
+    when 'bundler.version.simulated'
+      "Bundler is configured to simulate an incompatible version (expected:<4.0 actual:#{value})"
+    when 'bundler.unbundled'
+      "The Ruby process is not running in a bundle (no Gemfile found)"
+    when 'bundler.unlocked'
+      "The Ruby process has no dependency lock (no Gemfile.lock found)"
+    when 'bundler.frozen'
+      "Bundler is configured to freeze the dependency bundle"
+    when 'bundler.deployment'
+      "Bundler is configured in strict deployment mode"
+    when 'bundler.vendored'
+      "Bundler is configured to ignore gems out of the vendored path"
+    when 'fs.readonly'
+      "The Gemfile directory is read-only"
+    else
+      "Reason: '#{reason}'"
+    end
+  end
+
   def aborted(result)
     reason_class = result.size == 1 ? class_of(result.first[:reason]) : 'multiple_reasons'
-
-    # TODO: map codes to user-oriented text
-    reason_text = result.map { |r| r[:reason] }.join(', ')
+    reason_text = result.map { |r| text_for(r[:reason], r[:value]) }.compact.join(', ')
 
     { :result => 'abort', :result_reason => reason_text, :result_class => reason_class }
   end

61300e77d9cbe5970e71badd6886efe878e3ecc9
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2025-11-03 15:25:04 +0100

    Add state output information to stubs

--- test/fixtures/hot/stub.rb
+++ test/fixtures/hot/stub.rb
@@ -1,1 +1,11 @@
-puts "stub:hot"
+stub = File.basename(File.dirname(File.expand_path(__FILE__)))
+
+puts "stub:#{stub} start"
+puts "stub:#{stub} GEM_PATH:#{ENV['GEM_PATH'].inspect}"
+puts "stub:#{stub} Gem.path:#{Gem.path.inspect}"
+puts "stub:#{stub} deps:#{Gem.loaded_specs.map { |name, spec| [name, spec.version.to_s] }}"
+
+if Gem.loaded_specs['datadog']
+  require 'datadog'
+  puts "stub:#{stub} datadog:#{!!defined?(Datadog)}"
+end

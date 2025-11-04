d9c54acfa4d70212f57051c9a1e814da7054234a
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2025-11-04 16:24:31 +0100

    Fix testing on Ruby 1.8
    - `-r` is not supposed to have a space even though it can in some later
      Ruby versions
    - Before 1.9 `Gem` isn't present until `rubygems` is required

--- test/fixtures/group/stub.rb
+++ test/fixtures/group/stub.rb
@@ -1,6 +1,9 @@
 stub = File.basename(File.dirname(File.expand_path(__FILE__)))
 
 puts "stub:#{stub} start"
+
+require 'rubygems' unless defined?(Gem)
+
 puts "stub:#{stub} GEM_PATH:#{ENV['GEM_PATH'].inspect}"
 puts "stub:#{stub} Gem.path:#{Gem.path.inspect}"
 puts "stub:#{stub} deps:#{Gem.loaded_specs.map { |name, spec| [name, spec.version.to_s] }}"
